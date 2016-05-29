97962f19abd905f78cdb6f0b61bda7efbd1338b7
Firdous Ali <firdous_kind86@yahoo.com>
2016-05-29 12:35:35 +0500

    changes

--- app/pages/page1/page1.ts
+++ app/pages/page1/page1.ts
@@ -1,11 +1,29 @@
-import {Page} from 'ionic-angular';
-
+import {Page, NavController} from 'ionic-angular';
+import {GithubService} from '../../providers/github-service/github-service';
 
 @Page({
   templateUrl: 'build/pages/page1/page1.html',
+  providers: [GithubService]
 })
 export class Page1 {
-  constructor() {
+      public foundRepos;
+      public username;
 
-  }
+      constructor(private github: GithubService,
+                  private nav: NavController) {
+      }
+
+      getRepos() {
+          this.github.getRepos(this.username).subscribe(
+              data => {
+                  this.foundRepos = data.json();
+              },
+              err => console.error(err),
+              () => console.log('getRepos completed')
+          );
+      }
+      
+      goToDetails(repo) {
+          //this.nav.push(DetailsPage, { repo: repo });
+      }
 }
